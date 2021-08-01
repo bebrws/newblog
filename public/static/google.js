ebb39bd684afc623cb6fa569341480594f4cb4ae
Brad Barrows <bebrws@gmail.com>
2021-08-01 16:56:03 -0700

    add back navigations with a post and poject and static content

---
+++ public/static/google.js
@@ -0,0 +1,14 @@
+// Was for bebdev
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments)}
+gtag('js', new Date());
+
+gtag('config', 'UA-154810525-2');
+// I'll keep that tracker on for now I guess
+
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-154810525-1');
