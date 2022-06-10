c36c6e8c94a31f10a38ea5a5ef1b9cfc88f3547f
Brad Barrows <bradley.barrows@pluto.tv>
2022-06-09 20:17:06 -0700

    Change rewrite to redirects

--- next.config.js
+++ next.config.js
@@ -1,41 +1,60 @@
 module.exports = {
-  async rewrites() {
+  async redirects() {
     return [
       {
         source: '/posts/alacritty',
         destination: '/projects/alacritty-tabs',
+        permanent: true,
       },
       {
         source: '/posts/alacrittytabs',
         destination: '/projects/alacritty-tabs',
+        permanent: true,
       },
       {
         source: '/posts/kubernetesaliasesfunctions',
         destination: '/posts/kubernetes-aliases-functions',
+        permanent: true,
       },
       {
         source: '/posts/jsflatmapusage',
         destination: '/',
+        permanent: true,
       },
       {
         source: '/posts/pythonlistcomprehensionsinjs',
-      destination: '/posts/python-list-comp-js',
+        destination: '/posts/python-list-comp-js',
+        permanent: true,
       },
       {
         source: '/posts/chromedevtools',
         destination: '/',
+        permanent: true,
       },
       {
         source: '/posts/githubstats',
         destination: 'https://www.npmjs.com/package/githubrepostatistics',
+        permanent: true,
       },
       {
         source: '/blog/2019/12/14/wireguard',
         destination: '/posts/wireguard',
+        permanent: true,
+      },
+      {
+        source: '/blog/2019/12/14/wireguard/',
+        destination: '/posts/wireguard',
+        permanent: true,
+      },
+      {
+        source: '/blog/2019/07/01/system-preferences-security-and-privacy-osx',
+        destination: '/',
+        permanent: true,
       },
       {
         source: '/blog/2019/07/01/system-preferences-security-and-privacy-osx/',
         destination: '/',
+        permanent: true,
       },
     ]
   },
