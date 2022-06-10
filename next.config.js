445b9a9340e396e0a38dbfb93258af4481e79fa3
Brad Barrows <bradley.barrows@pluto.tv>
2022-06-09 20:08:33 -0700

    Redirects to fix HN backlinks

--- next.config.js
+++ next.config.js
@@ -1,4 +1,44 @@
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/posts/alacritty',
+        destination: '/projects/alacritty-tabs',
+      },
+      {
+        source: '/posts/alacrittytabs',
+        destination: '/projects/alacritty-tabs',
+      },
+      {
+        source: '/posts/kubernetesaliasesfunctions',
+        destination: '/posts/kubernetes-aliases-functions',
+      },
+      {
+        source: '/posts/jsflatmapusage',
+        destination: '/',
+      },
+      {
+        source: '/posts/pythonlistcomprehensionsinjs',
+      destination: '/posts/python-list-comp-js',
+      },
+      {
+        source: '/posts/chromedevtools',
+        destination: '/',
+      },
+      {
+        source: '/posts/githubstats',
+        destination: 'https://www.npmjs.com/package/githubrepostatistics',
+      },
+      {
+        source: '/blog/2019/12/14/wireguard',
+        destination: '/posts/wireguard',
+      },
+      {
+        source: '/blog/2019/07/01/system-preferences-security-and-privacy-osx/',
+        destination: '/',
+      },
+    ]
+  },
   pageExtensions: ["tsx"],
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push(
