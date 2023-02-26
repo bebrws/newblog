41ed2440b13676a9e556ba95a779babdc35bba7d
Bradley Barrows <bebrws@gmail.com>
2023-02-25 18:03:52 -0800

    analytics

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,6 +18,19 @@ export default function Layout({ children }: Props) {
         gtag('config', 'UA-154810525-2');
       `}
       </Script>
+
+
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-BZKS5D8HPF" />
+
+      <Script id="show-banner" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-BZKS5D8HPF');
+      `}
+      </Script>      
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
