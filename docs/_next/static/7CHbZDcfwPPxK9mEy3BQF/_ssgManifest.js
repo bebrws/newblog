828fda8a2203bd8e5600b415056a1b94bb2c12b3
Brad Barrows <bradley.barrows@pluto.tv>
2024-08-26 08:48:39 -0700

    Mon Aug 26 08:48:39 PDT 2024

---
+++ docs/_next/static/7CHbZDcfwPPxK9mEy3BQF/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fposts","\u002Fprojects","\u002F","\u002Fposts\u002Fpage\u002F[page]","\u002Fprojects\u002Fpage\u002F[page]","\u002Fposts\u002Ftags\u002F[[...slug]]","\u002Fprojects\u002Ftags\u002F[[...slug]]","\u002Fprojects\u002F[project]","\u002Fposts\u002F[post]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
